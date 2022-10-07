9feba729fa9c836caf9f4aa1f39c659995762363
Gruja <gruja.work@gmail.com>
2022-10-07 09:58:38 +0200

    blast use if flag is set
    blast_url

--- source/constants.js
+++ source/constants.js
@@ -2,17 +2,33 @@ const ethers = require("ethers");
 require("dotenv").config();
 const privateKey = process.env.PRIVATE_KEY;
 let etherprovider;
-if (process.env.NETWORK_ID == 420) {
-  etherprovider = new ethers.providers.JsonRpcProvider(
-    "https://goerli.optimism.io",
-    { chainId: Number(process.env.NETWORK_ID), name: process.env.NETWORK }
-  );
+
+var usingBlast =
+  process.env.USING_BLAST !== undefined
+    ? process.env.USING_BLAST === "true"
+    : false;
+
+if (usingBlast) {
+  console.log("BLAST!");
+  etherprovider = new ethers.providers.JsonRpcProvider(process.env.BLAST_URL, {
+    chainId: Number(process.env.NETWORK_ID),
+    name: process.env.NETWORK,
+  });
 } else {
-  etherprovider = new ethers.providers.InfuraProvider(
-    { chainId: Number(process.env.NETWORK_ID), name: process.env.NETWORK },
-    process.env.INFURA
-  );
+  console.log("INFURA!");
+  if (process.env.NETWORK_ID == 420) {
+    etherprovider = new ethers.providers.JsonRpcProvider(
+      "https://goerli.optimism.io",
+      { chainId: Number(process.env.NETWORK_ID), name: process.env.NETWORK }
+    );
+  } else {
+    etherprovider = new ethers.providers.InfuraProvider(
+      { chainId: Number(process.env.NETWORK_ID), name: process.env.NETWORK },
+      process.env.INFURA
+    );
+  }
 }
+
 let baseUrl = process.env.BASE_URL;
 
 module.exports = {
